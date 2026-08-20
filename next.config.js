7359cb71c865ef1c8d3d2df8ab803d7d684353ea
xioxhohan-ui <shohan@bestbuycart.com>
2026-08-20 20:59:37 +0600

    Remove conflicting static public/robots.txt and add gsap transpilePackages in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
+  transpilePackages: ['gsap'],
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
